73d380e6f65be33843c36189891efeddcc52e8d9
mahmadzadeh <mahmadzadeh@gmail.com>
2016-04-16 11:16:14 -0700

    Experimenting with graphing lib. Display some random data on screen

--- app/src/main/java/com/shapematchandroid/ui/ChartActivity.java
+++ app/src/main/java/com/shapematchandroid/ui/ChartActivity.java
@@ -1,18 +1,94 @@
 package com.shapematchandroid.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.shapematchandroid.R;
 
+import java.util.ArrayList;
 public class ChartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_chart);
+        setContentView(R.layout.chart_screen);
 
+        LineChart lineChart  = (LineChart) findViewById(R.id.line_chart);
+
+        setData(45, 100, lineChart);
+    }
+
+    private void setData(int count, float range, LineChart mChart) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 3;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals.add(new Entry(val, i));
+        }
+
+        LineDataSet set1;
+
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+
+            set1 = (LineDataSet)mChart.getData().getDataSetByIndex(0);
+            //set1.setYVals(yVals);
+            //mChart.getData().setXVals(xVals);
+            mChart.notifyDataSetChanged();
+
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(yVals, "DataSet 1");
+
+            // set1.setFillAlpha(110);
+            // set1.setFillColor(Color.RED);
+
+            // set the line to be drawn like this "- - - - - -"
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+
+//            if (Utils.getSDKInt() >= 18) {
+//                // fill drawable only supported on api level 18 and above
+//                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+//                set1.setFillDrawable(drawable);
+//            }
+//            else {
+                set1.setFillColor(Color.YELLOW);
+//            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(xVals, dataSets);
+
+            // set data
+            mChart.setData(data);
+        }
     }
 
 }
